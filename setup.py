d99e79453a342f21f7caa886e8aa790d5e8f2ebf
Mark Steve Samson <blazinggale@gmail.com>
2013-01-13 15:25:01 +0800

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='friendlists',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/marksteve/friendlists',
     license='MIT',
     author='Mark Steve Samson',
